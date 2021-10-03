a26a24d12caf8115de4e4f479b5a13ab87eb1212
windcalm666 <79242740+windcalm666@users.noreply.github.com>
2021-10-03 14:08:20 +0900

    4-2課題提出

---
+++ JavaStudy/HelloServlet/src/main/java/helloPackage/HelloServlet.java
@@ -0,0 +1,16 @@
+package helloPackage;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+public class HelloServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        response.setContentType("text/html; charset=utf-8");
+        PrintWriter out = response.getWriter();
+        out.println("<body>HelloServlet</body>");
+    }
+}
