cd7c245c2f2a51ebefa4380afb1400155abeb63a
windcalm666 <79242740+windcalm666@users.noreply.github.com>
2021-10-10 13:49:55 +0900

    5章チェックテスト提出

--- JavaStudy/demo/src/main/java/com/example/security/springsecurity/SpringsecurityApplication.java
+++ JavaStudy/demo/src/main/java/com/example/security/springsecurity/SpringsecurityApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.security.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SpringsecurityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SpringsecurityApplication.class, args);
 	}
 
 }
